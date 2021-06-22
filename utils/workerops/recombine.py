dcc9b616229ef48af6735c4a231e697e1af141ae
Jason Nucciarone <nucci.programming@gmail.com>
2021-06-22 13:53:59 +0000

    Add recombine to recomb dataset

---
+++ utils/workerops/recombine.py
@@ -0,0 +1,27 @@
+import pandas as pd
+import os
+
+
+def recombine(features, labels, save=False, **kwargs):
+    """Recombine a manipulated dataset back into a single pandas DataFrame.
+    
+    Keyword arguments:
+    features -- features of the manipulated dataset.
+    labels -- target labels of the manipulated dataset.
+    save -- save a copy of the recombined dataset in CSV format (default False).
+    **kwargs -> key: save_path -- path to save recombined dataset (not needed if save == False).
+    **kwargs -> key: manip_tag -- name of the data manipulation used on the dataset."""
+    # Recombine dataset using pd.concat
+    recomb = pd.concat([features, labels], axis=1, join="inner")
+
+    if save is False:
+        return recomb
+
+    else:
+        save_path = kwargs.get("save_path"); manip_tag = kwargs.get("manip_tag")
+
+        if os.path.exists(save_path) is False:
+            os.makedirs(save_path, exist_ok=True)
+
+        recomb.to_csv(save_path + "/" + manip_tag + ".csv", header=None)
+        return recomb
