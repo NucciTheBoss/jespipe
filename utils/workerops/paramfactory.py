1c15412ccf07b503bc4ddd476aa0e4c6f61d5e16
Jason Nucciarone <nucci.programming@gmail.com>
2021-07-13 21:16:22 +0000

    Setup worker 1 to use new manip plugin

--- utils/workerops/paramfactory.py
+++ utils/workerops/paramfactory.py
@@ -1,21 +1,62 @@
-import joblib
 import uuid
+from typing import List
+
+import joblib
+import numpy as np
+import pandas as pd
 
 
-def train_factory(name, model_name, dataframe, model_params, manip_params, save_path, manip_name, manip_tag, root_path):
-    """Generate parameter dictionary that will be sent out to the training plugins for the training stage.
-    Save as a pickle and return a file path reference to that pickle.
+def manip_factory(dataset_path: str, manip_tag: str, manip_params: str, save_path: str, 
+            tmp_path: str, root_path: str) -> str:
+    """
+    Create parameter dictionary that will be sent out to the user-specified manipulation plugin
+    in the training stage. Save the parameter dictionary as a pickle file.
+
+    ### Parameters:
+    :param dataset_path: System file path to dataset.
+    :param manip_tag: Tag to uniquely identify specific dataset manipulation.
+    :param manip_params: User-specified parameters for the data manipulation.
+    :param save_path: Where to save output data files.
+    :param tmp_path: System location of temp directory to store temporary files.
+    :param root_path: Root directory of Jespipe.
+
+    ### Returns:
+    :return: System file path reference to pickled parameter dictionary.
+    """
+    # Create root dictionary that will be converted to a pickle
+    d = dict()
+
+    # Create parameter dictionary
+    d["dataset"] = dataset_path; d["manip_tag"] = manip_tag; d["manip_params"] = manip_params
+    d["save_path"] = save_path; d["tmp_path"] = tmp_path
+
+    # Establish path to file in .tmp directory and dump dictionary
+    pickle_path = root_path + "/data/.tmp/" + str(uuid.uuid4()) + ".pkl"
+    joblib.dump(d, pickle_path)
     
-    Keyword arguments:
-    name -- name of the dataset.
-    model_name -- name of the model to be trained.
-    dataframe -- pandas DataFrame to be worked on.
-    model_params -- user specified hyperparameters for model being trained.
-    manip_params -- user specified parameters for the data manipulation.
-    save_path -- where to save manipulation specific model files.
-    manip_name -- name of the manipulation used on the pandas DataFrame.
-    manip_tag -- tag used to uniquely identify dataset manipulation.
-    root_path -- root directory of jespipe."""
+    return pickle_path
+
+
+def train_factory(name: str, model_name: str, dataframe: pd.DataFrame, model_params: dict, manip_params: dict, 
+                    save_path: str, manip_name: str, manip_tag: str, root_path: str) -> str:
+    """
+    Create parameter dictionary that will be sent out to the user-specified training plugin
+    in the training stage. Save the parameter dictionary as a pickle file.
+    
+    ### Parameters:
+    :param name: Name of the dataset.
+    :param model_name: Name to use for trained model.
+    :param dataframe: Pandas DataFrame to train the model on.
+    :param model_params: User-specified hyperparameters for the model being trained.
+    :param manip_params: User-specified parameters for the data manipulation.
+    :param save_path: Where to save output data files.
+    :param manip_name: name of the manipulation used on the pandas DataFrame.
+    :param manip_tag: Tag to uniquely identify specific dataset manipulation.
+    :param root_path: Root directory of Jespipe.
+
+    ### Returns:
+    :return: System file path reference to pickled parameter dictionary.
+    """
     # Create root dictionary that will be converted to a pickle
     d = dict()
     
@@ -37,17 +78,23 @@ def train_factory(name, model_name, dataframe, model_params, manip_params, save_
     return pickle_path
 
 
-def attack_factory(name, model_path, model_test_features, attack_params, save_path, root_path):
-    """Generate parameter dictionary that will be sent out to the attack plugins for the attack stage.
-    Save as a pickle and return a file path reference to that pickle.
+def attack_factory(name: str, model_path: str, model_test_features: np.ndarray, attack_params: dict, 
+                    save_path: str, root_path: str) -> str:
+    """
+    Create parameter dictionary that will be sent out to the user-specified attack plugin 
+    in the attack stage. Save the parameter dictionary as a pickle file.
     
-    Keyword arguments:
-    name -- name of the attack.
-    model_path -- path of model to attack.
-    model_test_features -- the data to manipulate.
-    attack_params -- parameters to use for the attack.
-    save_path -- where to save the adversarial examples.
-    root_path -- root directory of jespipe."""
+    ### Parameters:
+    :param name: Name of the attack.
+    :param model_path: System file path of model to attack.
+    :param model_test_features: The data to manipulate for the attack.
+    :param attack_params: Parameters to use for the attack.
+    :param save_path: System location save the adversarial examples.
+    :param root_path: Root directory of Jespipe.
+
+    ### Returns:
+    :return: System file path reference to pickled parameter dictionary.
+    """
     d = dict()
 
     d["name"] = name
@@ -65,16 +112,22 @@ def attack_factory(name, model_path, model_test_features, attack_params, save_pa
     return pickle_path
 
 
-def attack_train_factory(adver_features, model_labels, log_path, model_path, root_path):
-    """Generate parameter dictionary that will be sent out to the training plugins for the attack stage.
-    Save as a pickle and return a file path reference to that pickle.
+def attack_train_factory(adver_features: List[str], model_labels: np.ndarray, 
+                            log_path: str, model_path: str, root_path: str) -> str:
+    """
+    Create parameter dictionary that will be sent out to the user-specified training plugin 
+    in the attack stage. Save the parameter dictionary as a pickle file.
     
-    Keyword arguments:
-    adver_features -- list containing file path references to adversarial data.
-    model_labels -- the labels to evaluate the model on.
-    log_path -- where to save the data collected.
-    model_path -- file path to model.
-    root_path -- root directory of jespipe."""
+    ### Parameters:
+    :param adver_features: List containing system file path references to adversarial data for attack.
+    :param model_labels: The target feature(s) to evaluate the model on.
+    :param log_path: System location to save data collected on model during attack.
+    :param model_path: System file path of model.
+    :param root_path: Root directory of Jespipe.
+
+    ### Returns:
+    :return: System file path reference to pickled parameter dictionary.
+    """
     d = dict()
 
     d["adver_features"] = adver_features
@@ -89,11 +142,16 @@ def attack_train_factory(adver_features, model_labels, log_path, model_path, roo
     return pickle_path
 
 
-
-def clean_factory():
-    """Generate parameter dictionary that will be sent out to the cleaning plugins for the cleaning stage.
+def clean_factory() -> str:
+    """
+    Generate parameter dictionary that will be sent out to the cleaning plugins for the cleaning stage.
     Save as a pickle and return a file path reference to that pickle.
     
-    Keyword arguments:"""
+    ### Parameters:
+    - TODO
+
+    ### Returns:
+    :return: System file path reference to pickled parameter dictionary.
+    """
     # TODO: Update this function once you revisit the cleaning stage next week
     pass
